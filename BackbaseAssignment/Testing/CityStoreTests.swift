6554e168ddae3905d7d5a84e139ecaa06025ca42
Andrei Nevar <andrei@thecloseapp.com>
2020-04-22 13:52:54 +0200

    test runs but fails

--- BackbaseAssignment/Testing/CityStoreTests.swift
+++ BackbaseAssignment/Testing/CityStoreTests.swift
@@ -19,35 +19,25 @@ class CityStoreTests: QuickSpec {
         let store = CityStore(treeBuilder: treeBuilder, halt: true)
         var loadTreeFinished = false
 
-        beforeEach {
-            
-
-        }
-
-        // test doesn't work, and requires more time to fix. I tried a bunch of stuff:
-        // XCTest with XCTestExpectation, then imported Qcuik and Nimble
-        // ended up having to inject SceneDelegate, still no luck:
-        // it doesn't go inside the closure that searches for some reason.
-        // for SceneDelegate to start test app you need to reset content and settings
 
         context("should find cities") {
 
+            describe("insert + find") {
 
+                it("should find") {
+                    var rangeFound = 0..<1
+                    waitUntil (timeout: 500) { done in
+                        treeBuilder.buildTree(store.allCitiesArray, completed: { tree in
+                            store.setTree(tree)
+                            done()
+                        })
+                    }
 
-            it("should find") {
-                var rangeFound = 0..<1
-                waitUntil (timeout: 500) { done in
-                    treeBuilder.buildTree(store.allCitiesArray, completed: { tree in
-                        store.setTree(tree)
-
-                    })
-                }
-
-                waitUntil (timeout: 500) { done in
                     store.search(matching: "New York", handler: { range in
-                                           rangeFound = range
+                            rangeFound = range
                             expect((store.allCitiesArray[rangeFound]).contains(where: { $0.name == "New York"})).toEventually(beTrue())
                     })
+
                 }
             }
         }
